fcff722f136825f41b733b242ae749ec77bdf097
Clément Roche <rchclement@gmail.com>
2025-10-28 11:04:03 +0100

    examples

--- content/projects.js
+++ content/projects.js
@@ -1,8 +1,38 @@
 export const projects = [
+  // {
+  //   title: 'Deso',
+  //   source: 'Studio Freight',
+  //   href: 'https://deso.com',
+  // },
+  // {
+  //   title: 'Sculpting Harmony',
+  //   source: 'Resn',
+  //   href: 'https://gehry.getty.edu/',
+  // },
+  // {
+  //   title: 'Superpower',
+  //   source: '',
+  //   href: 'https://superpower.com/',
+  // },
+  // {
+  //   title: 'Daylight Computer',
+  //   source: 'Basement Studio',
+  //   href: 'https://daylightcomputer.com/',
+  // },
+  // {
+  //   title: 'Lifeworld by Olafur Eliasson',
+  //   source: '',
+  //   href: 'https://lifeworld.wetransfer.com/',
+  // },
   {
-    title: 'Deso',
-    source: 'Studio Freight',
-    href: 'https://deso.com',
+    title: 'Ibicash',
+    source: 'darkroom.engineering',
+    href: 'https://ibi.cash',
+  },
+  {
+    title: 'Grand Theft Auto VI',
+    source: 'Rockstar Games',
+    href: 'https://www.rockstargames.com/VI',
   },
   {
     title: 'Sculpting Harmony',
@@ -10,18 +40,18 @@ export const projects = [
     href: 'https://gehry.getty.edu/',
   },
   {
-    title: 'Superpower',
-    source: '',
-    href: 'https://superpower.com/',
+    title: 'Microsoft Design',
+    source: 'Microsoft',
+    href: 'https://microsoft.design/',
   },
   {
-    title: 'Daylight Computer',
-    source: 'Basement Studio',
-    href: 'https://daylightcomputer.com/',
+    title: 'Deso',
+    source: 'Studio Freight',
+    href: 'https://deso.com',
   },
   {
-    title: 'Lifeworld by Olafur Eliasson',
-    source: '',
-    href: 'https://lifeworld.wetransfer.com/',
+    title: 'Shopify Supply',
+    source: 'Shopify',
+    href: 'https://shopify.supply/',
   },
 ]
