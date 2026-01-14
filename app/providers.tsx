38d42f664253ec72f97020667ca6c6573748d77d
arzafran <franco@darkroom.engineering>
2026-01-14 13:57:37 -0300

    feat: add Vercel Speed Insights and Analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import dynamic from 'next/dynamic'
@@ -97,6 +99,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
       <RealViewport />
       {children}
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
