6ecf14cbd79d29835bbfde10ef7cfb2be838f378
arzafran <franco@darkroom.engineering>
2026-01-14 17:23:09 -0300

    refactor: use Cloudinary for videos (Mux free tier limited to 10)
    Switched videoProvider from Mux to Cloudinary. Mux can be re-enabled
    by changing one line in lib/media/providers/index.ts when needed.

--- lib/media/providers/index.ts
+++ lib/media/providers/index.ts
@@ -4,6 +4,6 @@ import { muxProvider } from './mux'
 
 // Provider configuration - easy to change later
 export const imageProvider: MediaProvider = cloudinaryProvider
-export const videoProvider: MediaProvider = muxProvider // Swap to cloudinaryProvider when needed
+export const videoProvider: MediaProvider = cloudinaryProvider // Using Cloudinary for videos (Mux free tier limited to 10)
 
 export { cloudinaryProvider, muxProvider }
